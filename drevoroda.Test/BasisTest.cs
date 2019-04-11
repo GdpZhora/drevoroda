042f574a8101e2926c1b904a57c50715fb98f6f6
Gdp_Zhora <gdp_zhora@mail.ru>
2019-04-12 03:03:39 +0500

    Переделал тест с помощью Moq

--- drevoroda.Test/BasisTest.cs
+++ drevoroda.Test/BasisTest.cs
@@ -3,6 +3,8 @@ using Xunit;
 using System.Collections.Generic;
 using drevoroda.Models;
 using drevoroda.Controllers;
+using System.Linq;
+using Moq;
 
 namespace drevoroda.Test
 {
@@ -12,22 +14,21 @@ namespace drevoroda.Test
         public void ListRepoPerson()
         {
 
-            RepoPerson repoPerson = new RepoPerson();
-            PersonController controller = new PersonController(repoPerson);
-
-
-        }
-    }
-    public class RepoPerson : IPersonRepository
-    {
-        public IEnumerable<Person> Persons => new List<Person>
-        {
+            Mock<IPersonRepository> mock = new Mock<IPersonRepository>();
+            mock.Setup(m => m.Persons).Returns(new Person[] {
                 new Person { FullName = "Низамеева Антонина Васильевна", DateOfBirth = new DateTime(1941, 01, 01), PlaceOfBirth = "СССР, Россия, Оренбургская область, с. Аниховка" },
                 new Person { FullName = "Низамеев Николай Мисбахович", DateOfBirth = new DateTime(1941, 01, 01), PlaceOfBirth = "СССР, Россия, Оренбургская область, с. Аниховка" },
                 new Person { FullName = "Гаврусь Александра", DateOfBirth = new DateTime(1941, 01, 01), PlaceOfBirth = "СССР, Россия, Оренбургская область, с. Аниховка" },
                 new Person { FullName = "Гаврусь Петр Сарапионович", DateOfBirth = new DateTime(1941, 01, 01), PlaceOfBirth = "СССР, Россия, Оренбургская область, с. Аниховка" }
+            });
 
-        };
+            PersonController controller = new PersonController(mock.Object);
+            IEnumerable<Person> result = controller.List().ViewData.Model as IEnumerable<Person>;
+
+            Person[] people = result.ToArray();
+
+            Assert.True(people.Length == 4);
+        }
     }
 
 }
